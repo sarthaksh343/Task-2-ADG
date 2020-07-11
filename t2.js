f43dce1a8dfc098a2d742101f7cb3b7e0faaf74a
sarthaksh343 <sarthaksh343@gmail.com>
2020-07-11 20:27:59 +0530

    first commit

---
+++ t2.js
@@ -0,0 +1,89 @@
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+ 
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+ 
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+ 
+    this.questionIndex++;
+}
+ 
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+ 
+ 
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+ 
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
+ 
+ 
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+ 
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+ 
+        showProgress();
+    }
+};
+ 
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+ 
+ 
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+ 
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+ 
+var questions = [
+    new Question("Naruto's sirname is taken from?", ["Father", "Mother","Teacher", "None"], "Mother"),
+    new Question("Itachi's sharingan's ability?", ["Amaterasu", "Kotoamatsukai", "Blaze", "Kamui"], "Amaterasu"),
+    new Question("Hinata Posseses which eye?", ["Sharingan", "Byakugan","Rinnegan", "RinneSharingan"], "Byakugan"),
+    new Question("Which jutsu Kagura never saw?", ["Shodow clone jutsu", "Tailed beast Bomb", "Rasengan", "Harem Jutsu"], "Harem Jutsu"),
+    new Question("Naruto uses which word always?", ["Dattebayo", "Datebeyo", "Dattevayo", "All"], "Dattebayo")
+];
+ 
+var quiz = new Quiz(questions);
+
+
+ 
+populate();
+
